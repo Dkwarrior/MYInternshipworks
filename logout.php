e055270c5be48adb44c70560b9b21e5bca4f58ef
Deepak Dhruw <67164487+Dkwarrior@users.noreply.github.com>
2023-12-26 20:11:55 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php $page="dummy" ?>
-<?php require_once 'header.php';?>
-<?php
-unset($_SESSION["email"]);
-unset($_SESSION["role"]);
-session_destroy();
-redirect("login.php");
-?>
-<?php require_once 'footer.php';?>
